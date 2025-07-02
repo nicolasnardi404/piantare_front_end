3b3618644504cca275e450069e0fa3cc42d71993
esquizo ciborgue <nicolasnardi404@gmail.com>
2025-07-02 14:50:24 +0200

    fix bugs with following mouse effects

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -125,7 +125,7 @@ const Particles = ({
     };
 
     if (moveParticlesOnHover) {
-      container.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mousemove", handleMouseMove);
     }
 
     const count = particleCount;
@@ -211,7 +211,7 @@ const Particles = ({
     return () => {
       window.removeEventListener("resize", resize);
       if (moveParticlesOnHover) {
-        container.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mousemove", handleMouseMove);
       }
       cancelAnimationFrame(animationFrameId);
       if (container.contains(gl.canvas)) {
